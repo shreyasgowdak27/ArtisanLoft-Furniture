ad7398c00c921b47a51345f93ede1620636c9f0f
shreyasgowdak27 <gshreyas151@gmail.com>
2026-02-22 13:25:04 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://artisanloft-furniture.onrender.com/api',
 });
 
 export const getProducts = () => api.get('/products');
